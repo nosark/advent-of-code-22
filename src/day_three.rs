6f4afe9e8e93d83f3f0347a3335e2423da821c5d
Kyle Nosar <kylenosar@gmail.com>
2023-01-04 17:40:39 -0800

    day 3 part one solved plus test

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, sync::Mutex};
 
 /// Need to read each line and split line into two strings ( compartments )
 /// Then, we compare each string to identify the item that is duplicated in both
@@ -12,6 +12,27 @@ pub fn prepare_input(input: &str) -> Vec<(&str, &str)> {
         .map(split_string_into_compartments)
         .collect::<Vec<(&str, &str)>>()
 }
+
+pub fn group_lines_by_threes(input: &str) -> u32 {
+    let mut score:u32  = 0;
+    let lines: Vec<&str> = input.lines().collect();
+    let mut line_buf = Vec::<&str>::with_capacity(3);
+    for i in 0..lines.len() {
+        if i + 1 % 3 == 0 {
+            score += get_score_of_badge(&line_buf);
+            line_buf.clear();
+        }
+        line_buf.push(lines[i]);
+    }
+
+    score
+}
+
+fn get_score_of_badge(value: &Vec<&str>) -> u32 {
+    let mut char_set = HashSet::<char>::new();
+    unimplemented!()    
+}
+
 
 fn split_string_into_compartments(value: &str) -> (&str, &str) {
     let middle = value.len() / 2;
@@ -55,12 +76,19 @@ mod tests {
     use super::prepare_input;
 
     const SAMPLE_TEST_CASE: &str = include_str!("../res/tests/day3_sample_test.txt");
-
+    const INPUT: &str = include_str!("../res/day_three_input.txt");
     #[test]
     fn day_three_sample_input_test() {
         let input = prepare_input(SAMPLE_TEST_CASE);
         let result = get_item_priority_score(input).unwrap();
 
         assert_eq!(result, 157)
+    }
+
+    fn day_three_solve() {
+        let input = prepare_input(INPUT);
+        let result = get_item_priority_score(input).unwrap();
+
+        assert_eq!(result, 7824)
     }
 }
