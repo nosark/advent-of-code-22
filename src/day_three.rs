c3a1fae6e9f4f8c91e5151402918c454221b879d
Kyle Nosar <kylenosar@gmail.com>
2023-09-27 18:46:37 -0700

    finished day 3 w/ tests

--- src/day_three.rs
+++ src/day_three.rs
@@ -13,47 +13,8 @@ pub fn prepare_input(input: &str) -> Vec<(&str, &str)> {
         .collect::<Vec<(&str, &str)>>()
 }
 
-pub fn group_lines_by_threes(input: &str) -> u32 {
-    let mut score:u32  = 0;
-    let lines: Vec<&str> = input.lines().collect();
-    let mut line_buf = Vec::<&str>::with_capacity(3);
-    for i in 0..lines.len() {
-        if i + 1 % 3 == 0 {
-            score += get_score_of_badge(&line_buf);
-            line_buf.clear();
-        }
-        line_buf.push(lines[i]);
-    }
-
-    score
-}
-
-fn get_score_of_badge(values: &Vec<&str>) -> u32 {
-    let mut char_set = HashMap::<char, u32>::new();
-    let mut score = 0;
-    let mut found_flag = false; 
-    for value in values {
-        for letter in value.chars() {
-            char_set.entry(letter)
-                .and_modify(|e| {
-                    *e += 1;
-                    if *e == 3 {
-                        score = get_badge_score(letter as u8);
-                        found_flag = true;
-                    }
-                })
-                .or_insert(1);
-                if found_flag {
-                    break;
-                } 
-        }
-    }
-
-    score
-}
-
 fn get_badge_score(value: u8) -> u32 {
-    let mut score = 0;
+    let score;
     match value.is_ascii_lowercase() {
         true => score = value as u32 - 96,
         false => score = value as u32 - 38,
@@ -94,11 +55,55 @@ fn find_unique_duplicate(value: (&str, &str)) -> u32 {
     score
 }
 
+fn find_unique_duplicate_for_three(value: (&str, &str, &str)) -> u32 {
+    let mut char_set = HashSet::<u8>::new();
+    let mut score = 0;
+    let mut possible_uniques = Vec::<char>::new();
+
+    // need to get possible duplicates from first two strings
+    // take a vec of those possible duplicates and search third string
+
+    for c in value.0.bytes() {
+        char_set.insert(c);
+    }
+
+    for ch in value.1.bytes() {
+        if char_set.contains(&ch) {
+            possible_uniques.push(ch as char);
+        }
+    }
+
+    for letter in possible_uniques {
+        if value.2.contains(letter) {
+            score = get_badge_score(letter as u8);
+        }
+    }
+    score
+
+}
+
+fn get_item_priority_score_group_threes(input: &str) -> u32 {
+    let mut score : u32 = 0;
+    let lines: Vec<&str> = input.lines().collect();
+    let mut i = 0;
+    loop {
+        score += find_unique_duplicate_for_three((lines[i as usize], lines[i+1 as usize], lines[i+2 as usize]));
+        if i >= lines.len()-3 {
+            break;
+        } else {
+            i += 3;
+        }
+    }
+
+    score
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::day_three::get_item_priority_score_group_threes;
+
     use super::get_item_priority_score;
     use super::prepare_input;
-    use super::group_lines_by_threes;
 
     const SAMPLE_TEST_CASE: &str = include_str!("../res/tests/day3_sample_test.txt");
     const INPUT: &str = include_str!("../res/day_three_input.txt");
@@ -120,7 +125,13 @@ mod tests {
 
     #[test]
     fn day_three_part_2_solve() {
-        let result = group_lines_by_threes(INPUT);
-        assert_eq!(result, 7824)
+        let result = get_item_priority_score_group_threes(INPUT);
+        assert_eq!(result, 2798)
+    }
+
+    #[test]
+    fn day_three_sample_test_two() {
+        let result = get_item_priority_score_group_threes(SAMPLE_TEST_CASE);
+        assert_eq!(result, 70)
     }
 }
